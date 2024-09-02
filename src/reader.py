5e1563bf2aa36f059b507fc0c5329fe27b739059
anaclaragelabert <133994327+anaclaragelabert@users.noreply.github.com>
2024-09-02 10:12:16 -0300

    holi

---
+++ src/reader.py
@@ -0,0 +1,16 @@
+import csv
+
+# Ruta al archivo CSV
+file_path = 'datos\questions.csv'
+
+# Abrir y leer el archivo CSV
+with open(file_path, newline='', encoding='latin1') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # Leer y mostrar la cabecera
+    headers = next(reader)
+    print("Cabeceras:", headers)
+
+    # Leer cada fila del CSV
+    #for row in reader:
+    #   print(row)
